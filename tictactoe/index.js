2d75fd8cb43a7e53b82fc1198dfcbe74ed137a6a
turnertop <turnerfool@pm.me>
2023-03-03 14:28:39 -0800

    secret

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -1,5 +1,5 @@
 // so if i make tictactoe game i wont end up like ....
-
+// fuck ima end up like frank this is too hard javascript is out of my forte for my brain omg omg omg im not a developer type guy im like just average joe
 
 
 
@@ -9,7 +9,12 @@ let playBoard = ["", "", "", "", "", "", "", "", ""];
 
 const boardContainer = document.querySelector(".play-area");
 
+const renderBoard = () => {
+	boardContainer.innerHTML = "";
+	
 
+
+};
 
 
 
