3d53617cb31671ee4131219af4c56bd583ccb51a
turnertop <turnerfool@pm.me>
2023-03-03 15:32:19 -0800

    fuck ima end up like frank

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -14,7 +14,8 @@ const boardContainer = document.querySelector(".play-area");
 // thank sam altman for chatgpt otherwise average joe like me born weak born inferior can close the gap with laputas!!!!!
 const renderBoard = () => {
 	boardContainer.innerHTML = "";
-	playBoard((e, i) 
+	playBoard((e, i) => {
+		
 
 
 
@@ -22,8 +23,18 @@ const renderBoard = () => {
 
 
 };
-
-
+/*
+const render_board = () => {
+  let html = "";
+  for (let i = 0; i < play_board.length; i++) {
+    const value = play_board[i];
+    const isOccupied = value == player || value == computer;
+    const cssClass = isOccupied ? "block occupied" : "block";
+    html += `<div id="block_${i}" class="${cssClass}" onclick="addPlayerMove(${i})">${value}</div>`;
+  }
+  board_container.innerHTML = html;
+};
+*/
 
 // is it grippy?
 // is it pink?
